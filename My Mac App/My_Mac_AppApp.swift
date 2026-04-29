b6fee3274b37f85b787085055bf47ddb65fa0e51
Teja Karlapudi <teja2495@gmail.com>
2026-04-28 20:44:51 -0400

    Initial Commit

---
+++ My Mac App/My_Mac_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Mac_AppApp.swift
+//  My Mac App
+//
+//  Created by Teja Karlapudi on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct My_Mac_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
